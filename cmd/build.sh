fee4e5b671dcb70af5dfe87b271d4f6bcb1f6e8e
ldyxgc <ldyxgc@163.com>
2024-12-09 05:25:35 +0800

    Add cmd/build.sh and .github/workflows/build.yml

---
+++ cmd/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x # Print commands and their arguments as they are executed.
+
+# Clean
+rm -rf build
+
+# Config
+cmake -B build \
+  $@
+if [ $? -ne 0 ]; then exit 1; fi
+
+# Build
+cmake --build build \
+  --parallel
+if [ $? -ne 0 ]; then exit 1; fi
