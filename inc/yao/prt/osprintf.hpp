d8a7d162e47b9a8c33688de041429ea9a27a87b3
ldyxgc <ldyxgc@163.com>
2024-12-10 04:34:28 +0800

    Add YAO_CONFIG__OSPRINTF, update yao{prt{osprintf()}}

--- inc/yao/prt/osprintf.hpp
+++ inc/yao/prt/osprintf.hpp
@@ -1,6 +1,10 @@
 #ifndef __YAO__PRT__OSPRINTF__HPP__
 #define __YAO__PRT__OSPRINTF__HPP__
 
+#include "yao/config.hpp"
+
+#if YAO_CONFIG__OSPRINTF == YAO_CONFIG__OSPRINTF__FUNCTION
+
 #include "yao/prt/impl/func/osprintf.hpp"
 
 namespace yao::prt {
@@ -8,5 +12,22 @@ namespace yao::prt {
 constexpr auto osprintf = impl::func::osprintf;
 
 } // namespace yao::prt
+
+#elif YAO_CONFIG__OSPRINTF == YAO_CONFIG__OSPRINTF__TEMPLATE
+
+#include <ostream>
+
+#include "yao/help.hpp"
+#include "yao/prt/impl/tmpl/osprintf.hpp"
+
+namespace yao::prt {
+
+void osprintf(std::ostream &os, not_null<czstring> fmt, auto... args) {
+  impl::tmpl::osprintf(os, fmt, args...);
+}
+
+} // namespace yao::prt
+
+#endif
 
 #endif
