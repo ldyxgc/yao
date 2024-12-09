a8dc8ed5469110119132587f24d87cbe89276023
ldyxgc <ldyxgc@163.com>
2024-12-09 19:04:30 +0800

    Update cmd/*.sh and .github/workflows/*.yml

---
+++ cmd/debug.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x # Print commands and their arguments as they are executed.
+
+# Option
+VAR__CMAKE_BUILD_TYPE=Debug
+
+# Clean
+rm -rf build
+
+# Config
+cmake -B build \
+  $@ \
+  -D CMAKE_BUILD_TYPE=${VAR__CMAKE_BUILD_TYPE}
+if [ $? -ne 0 ]; then exit 1; fi
+
+# Build
+cmake --build build \
+  --config ${VAR__CMAKE_BUILD_TYPE} \
+  --parallel
+if [ $? -ne 0 ]; then exit 1; fi
+
+# Test
+ctest --test-dir build \
+  --build-config ${VAR__CMAKE_BUILD_TYPE} \
+  --parallel
+if [ $? -ne 0 ]; then exit 1; fi
--- cmd/release.sh
+++ cmd/release.sh
@@ -2,20 +2,26 @@
 
 set -x # Print commands and their arguments as they are executed.
 
+# Option
+VAR__CMAKE_BUILD_TYPE=Release
+
 # Clean
 rm -rf build
 
 # Config
 cmake -B build \
-  $@
+  $@ \
+  -D CMAKE_BUILD_TYPE=${VAR__CMAKE_BUILD_TYPE}
 if [ $? -ne 0 ]; then exit 1; fi
 
 # Build
 cmake --build build \
+  --config ${VAR__CMAKE_BUILD_TYPE} \
   --parallel
 if [ $? -ne 0 ]; then exit 1; fi
 
 # Test
 ctest --test-dir build \
+  --build-config ${VAR__CMAKE_BUILD_TYPE} \
   --parallel
 if [ $? -ne 0 ]; then exit 1; fi
