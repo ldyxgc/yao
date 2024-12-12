328f1ab79acc09dba6fcbd8ae7677b6211bf6807
ldyxgc <ldyxgc@163.com>
2024-12-12 15:46:17 +0800

    Add yao{re{stt{VirtualStateBase{},VirtualState{}}}}

---
+++ exe/yao/re/stt/yao__re__stt__VirtualState.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <memory>
+
+#include "yao/prt/osprint_value.hpp"
+#include "yao/re/stt/ConcatState.hpp"
+#include "yao/re/stt/EpsilonState.hpp"
+#include "yao/re/stt/KleeneState.hpp"
+#include "yao/re/stt/SymbolState.hpp"
+#include "yao/re/stt/UnionState.hpp"
+#include "yao/re/stt/VirtualState.hpp"
+#include "yao/re/stt/VirtualStateBase.hpp"
+
+int main() {
+
+  using std::cout;
+  using yao::prt::osprint_value;
+
+  using EpsilonState = yao::re::stt::EpsilonState<int>;
+  using SymbolState = yao::re::stt::SymbolState<int>;
+  using KleeneState = yao::re::stt::KleeneState<SymbolState>;
+  using ConcatState = yao::re::stt::ConcatState<SymbolState, SymbolState>;
+  using UnionState = yao::re::stt::UnionState<SymbolState, SymbolState>;
+
+  using VirtualStateBase = yao::re::stt::VirtualStateBase<int>;
+  using yao::re::stt::VirtualState;
+
+  using EpsilonVirtualState = VirtualState<EpsilonState>;
+  using SymbolVirtualState = VirtualState<SymbolState>;
+  using KleeneVirtualState = VirtualState<KleeneState>;
+  using ConcatVirtualState = VirtualState<ConcatState>;
+  using UnionVirtualState = VirtualState<UnionState>;
+
+  auto print_value = []<bool ns = false, bool tp = false>(const auto &value) {
+    osprint_value<ns, tp>(cout, value);
+    cout << '\n';
+  };
+  auto print_state = [&](const auto &state) {
+    print_value(state);
+    cout << "is_final: " << state.is_final() << ", is_dead: " << state.is_dead()
+         << '\n';
+  };
+  auto match_print = [&](auto &state, auto symbol) {
+    state.match(symbol);
+    cout << ">> ";
+    osprint_value(cout, symbol);
+    cout << '\n';
+    print_state(state);
+  };
+
+  {
+    EpsilonVirtualState epsilon_virtual_state{};
+    print_value.operator()(epsilon_virtual_state);
+    print_value.operator()<true>(epsilon_virtual_state);
+    print_value.operator()<false, true>(epsilon_virtual_state);
+    print_value.operator()<true, true>(epsilon_virtual_state);
+  }
+  cout << '\n';
+  {
+    EpsilonVirtualState epsilon_virtual_state{};
+    VirtualStateBase &virtual_state_epsilon = epsilon_virtual_state;
+    print_value.operator()(virtual_state_epsilon);
+    print_value.operator()<true>(virtual_state_epsilon);
+    print_value.operator()<false, true>(virtual_state_epsilon);
+    print_value.operator()<true, true>(virtual_state_epsilon);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<EpsilonVirtualState> epsilon_virtual_state{
+        EpsilonVirtualState::make_uptr()};
+    print_value.operator()(epsilon_virtual_state);
+    print_value.operator()<true>(epsilon_virtual_state);
+    print_value.operator()<false, true>(epsilon_virtual_state);
+    print_value.operator()<true, true>(epsilon_virtual_state);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_epsilon{
+        EpsilonVirtualState::make_uptr()};
+    print_value.operator()(virtual_state_epsilon);
+    print_value.operator()<true>(virtual_state_epsilon);
+    print_value.operator()<false, true>(virtual_state_epsilon);
+    print_value.operator()<true, true>(virtual_state_epsilon);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_epsilon{
+        EpsilonVirtualState::make_uptr()};
+    print_state(*virtual_state_epsilon);
+    match_print(*virtual_state_epsilon, 0);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_symbol{
+        SymbolVirtualState::make_uptr(0)};
+    print_state(*virtual_state_symbol);
+    match_print(*virtual_state_symbol, 0);
+    match_print(*virtual_state_symbol, 0);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_kleene{
+        KleeneVirtualState::make_uptr(SymbolState{0})};
+    print_state(*virtual_state_kleene);
+    match_print(*virtual_state_kleene, 0);
+    match_print(*virtual_state_kleene, 0);
+    match_print(*virtual_state_kleene, 1);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_concat{
+        ConcatVirtualState::make_uptr(SymbolState{0}, SymbolState{1})};
+    print_state(*virtual_state_concat);
+    match_print(*virtual_state_concat, 0);
+    match_print(*virtual_state_concat, 1);
+    match_print(*virtual_state_concat, 0);
+  }
+  cout << '\n';
+  {
+    std::unique_ptr<VirtualStateBase> virtual_state_union{
+        UnionVirtualState::make_uptr(SymbolState{0}, SymbolState{1})};
+    print_state(*virtual_state_union);
+    match_print(*virtual_state_union, 0);
+    match_print(*virtual_state_union, 0);
+  }
+
+  return 0;
+}
---
+++ inc/yao/re/stt/VirtualState.hpp
@@ -0,0 +1,74 @@
+#ifndef __YAO__RE__STT__VIRTUAL_STATE__HPP__
+#define __YAO__RE__STT__VIRTUAL_STATE__HPP__
+
+#include <compare>
+#include <concepts>
+#include <memory>
+#include <ostream>
+
+#include "yao/def/warn.hpp"
+#include "yao/help.hpp"
+#include "yao/re/stt/StateBase.hpp"
+#include "yao/re/stt/StateType.hpp"
+#include "yao/re/stt/VirtualStateBase.hpp"
+#include "yao/re/stt/c_o_State.hpp"
+
+namespace yao::re::stt {
+
+YAO_WARN_PUSH
+YAO_WARN_OFF__PADDING
+
+template <typename ConcreteState>
+  requires c_o_State<ConcreteState>
+class VirtualState : public VirtualStateBase<typename ConcreteState::Symbol>,
+                     private StateBase<VirtualState<ConcreteState>> {
+public:
+  using Symbol = typename ConcreteState::Symbol;
+
+public:
+  VirtualState(const auto &...args);
+  static not_null<owner<VirtualState *>> make_rptr(const auto &...args);
+  static not_null<std::unique_ptr<VirtualState>> make_uptr(const auto &...args);
+  not_null<owner<VirtualState *>> copy_rptr() const override;
+  not_null<std::unique_ptr<VirtualState>> copy_uptr() const;
+
+  bool operator==(const VirtualState &rhs) const;
+  auto operator<=>(const VirtualState &rhs) const;
+  template <typename OtherConcreteState>
+    requires c_o_State<OtherConcreteState> &&
+             std::same_as<typename OtherConcreteState::Symbol,
+                          typename ConcreteState::Symbol> &&
+             (!std::same_as<OtherConcreteState, ConcreteState>)
+  bool operator==(const VirtualState<OtherConcreteState> &rhs) const;
+  template <typename OtherConcreteState>
+    requires c_o_State<OtherConcreteState> &&
+             std::same_as<typename OtherConcreteState::Symbol,
+                          typename ConcreteState::Symbol> &&
+             (!std::same_as<OtherConcreteState, ConcreteState>)
+  auto operator<=>(const VirtualState<OtherConcreteState> &rhs) const;
+  bool operator==(const VirtualStateBase<Symbol> &rhs) const override;
+  std::strong_ordering
+  operator<=>(const VirtualStateBase<Symbol> &rhs) const override;
+
+  StateType type() const override;
+
+  void match(const Symbol &symbol) override;
+  bool is_final() const override;
+  bool is_dead() const override;
+
+  template <bool ns = false, bool tp = false>
+  static void osprint_type(std::ostream &os);
+  void osprint_value(std::ostream &os, bool ns = false,
+                     bool tp = false) const override;
+
+private:
+  ConcreteState _concrete_state;
+};
+
+YAO_WARN_POP
+
+} // namespace yao::re::stt
+
+#include "yao/re/stt/VirtualState.ipp"
+
+#endif
