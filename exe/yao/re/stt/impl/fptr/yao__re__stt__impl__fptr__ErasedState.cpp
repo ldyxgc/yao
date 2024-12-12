d3bbeb4e587a063b2d88397c9f2a68317348cb0c
ldyxgc <ldyxgc@163.com>
2024-12-13 04:37:06 +0800

    Add yao::re::stt::impl::fptr::ErasedState{}

---
+++ exe/yao/re/stt/impl/fptr/yao__re__stt__impl__fptr__ErasedState.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <iostream>
+#include <ratio>
+
+#include "yao/re/stt/ConcatState.hpp"
+#include "yao/re/stt/EpsilonState.hpp"
+#include "yao/re/stt/KleeneState.hpp"
+#include "yao/re/stt/SymbolState.hpp"
+#include "yao/re/stt/impl/fptr/ErasedState.hpp"
+
+int main() {
+
+  using ErasedState = yao::re::stt::impl::fptr::ErasedState<int>;
+  const std::size_t CNT_MAX = 1024 * 32;
+  const int NUM_MAX = 10;
+
+  using EpsilonState = yao::re::stt::EpsilonState<int>;
+  using SymbolState = yao::re::stt::SymbolState<int>;
+  using KleeneErasedState = yao::re::stt::KleeneState<ErasedState>;
+  using ConcatErasedState = yao::re::stt::ConcatState<ErasedState, ErasedState>;
+
+  ErasedState re = EpsilonState{};
+  for (int i = 0; i <= NUM_MAX; i++)
+    re = ConcatErasedState{re, KleeneErasedState{SymbolState{i}}};
+
+  auto clk_bgn = std::chrono::high_resolution_clock::now();
+  {
+    for (int i = 0; i <= NUM_MAX; i++)
+      for (std::size_t cnt = 0; cnt < CNT_MAX; cnt++)
+        re.match(i);
+  }
+  auto clk_end = std::chrono::high_resolution_clock::now();
+
+  std::chrono::duration<double, std::milli> clk_dur{clk_end - clk_bgn};
+  std::cout << "time: " << clk_dur.count() << "ms\n";
+  std::cout << "is_final: " << re.is_final() << '\n';
+
+  return 0;
+}
