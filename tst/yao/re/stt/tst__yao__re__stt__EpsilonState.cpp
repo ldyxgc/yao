f9c2d8d4d05a43d66ba5517cc4def76e48bdd104
ldyxgc <ldyxgc@163.com>
2024-12-18 20:59:19 +0800

    Update tests of yao{re{stt{EpsilonState{},SymbolState{}}}}

--- tst/yao/re/stt/tst__yao__re__stt__EpsilonState.cpp
+++ tst/yao/re/stt/tst__yao__re__stt__EpsilonState.cpp
@@ -1,12 +1,16 @@
+#include <memory>
+
 #include "yao/def/check.hpp"
 #include "yao/re/stt/EpsilonState.hpp"
+#include "yao/re/stt/State.hpp"
 
 int main() {
 
+  using State = yao::re::stt::State<int>;
   using EpsilonState = yao::re::stt::EpsilonState<int>;
 
   {
-    auto epsilon_state = EpsilonState::make();
+    std::unique_ptr<EpsilonState> epsilon_state = EpsilonState::make();
     YAO_CHECK(epsilon_state->is_final() == true);
     YAO_CHECK(epsilon_state->is_dead() == false);
 
@@ -17,6 +21,20 @@ int main() {
     epsilon_state->match(0);
     YAO_CHECK(epsilon_state->is_final() == false);
     YAO_CHECK(epsilon_state->is_dead() == true);
+  }
+
+  {
+    std::unique_ptr<State> state = EpsilonState::make();
+    YAO_CHECK(state->is_final() == true);
+    YAO_CHECK(state->is_dead() == false);
+
+    state->match(0);
+    YAO_CHECK(state->is_final() == false);
+    YAO_CHECK(state->is_dead() == true);
+
+    state->match(1);
+    YAO_CHECK(state->is_final() == false);
+    YAO_CHECK(state->is_dead() == true);
   }
 
   return 0;
--- tst/yao/re/stt/tst__yao__re__stt__SymbolState.cpp
+++ tst/yao/re/stt/tst__yao__re__stt__SymbolState.cpp
@@ -1,12 +1,16 @@
+#include <memory>
+
 #include "yao/def/check.hpp"
+#include "yao/re/stt/State.hpp"
 #include "yao/re/stt/SymbolState.hpp"
 
 int main() {
 
+  using State = yao::re::stt::State<int>;
   using SymbolState = yao::re::stt::SymbolState<int>;
 
   {
-    auto symbol_state = SymbolState::make(0);
+    std::unique_ptr<SymbolState> symbol_state = SymbolState::make(0);
     YAO_CHECK(symbol_state->is_final() == false);
     YAO_CHECK(symbol_state->is_dead() == false);
 
@@ -24,17 +28,17 @@ int main() {
   }
 
   {
-    auto symbol_state = SymbolState::make(0);
-    YAO_CHECK(symbol_state->is_final() == false);
-    YAO_CHECK(symbol_state->is_dead() == false);
+    std::unique_ptr<State> state = SymbolState::make(0);
+    YAO_CHECK(state->is_final() == false);
+    YAO_CHECK(state->is_dead() == false);
 
-    symbol_state->match(1);
-    YAO_CHECK(symbol_state->is_final() == false);
-    YAO_CHECK(symbol_state->is_dead() == true);
+    state->match(1);
+    YAO_CHECK(state->is_final() == false);
+    YAO_CHECK(state->is_dead() == true);
 
-    symbol_state->match(0);
-    YAO_CHECK(symbol_state->is_final() == false);
-    YAO_CHECK(symbol_state->is_dead() == true);
+    state->match(0);
+    YAO_CHECK(state->is_final() == false);
+    YAO_CHECK(state->is_dead() == true);
   }
 
   return 0;
