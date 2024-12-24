f641a6844bb34fd5436e5ad56263a9e69d114de3
ldyxgc <ldyxgc@163.com>
2024-12-25 03:17:34 +0800

    Suppress MSVC abort() message when "YAO_CLAIM(false);"

--- inc/yao/def/claim.hpp
+++ inc/yao/def/claim.hpp
@@ -10,11 +10,23 @@
 
 #include "yao/prt/osprintf.hpp"
 
+#if YAO_CONFIG__COMPILER == YAO_CONFIG__COMPILER__MSVC
+
+#define YAO_CLAIM(expr)                                                        \
+  ((expr) ? void(0)                                                            \
+          : (yao::prt::osprintf(std::cerr, "%s:%i: Claim `%s` failed\n",       \
+                                __FILE__, __LINE__, #expr),                    \
+             _set_abort_behavior(0, _WRITE_ABORT_MSG), std::abort()))
+
+#else
+
 #define YAO_CLAIM(expr)                                                        \
   ((expr) ? void(0)                                                            \
           : (yao::prt::osprintf(std::cerr, "%s:%i: Claim `%s` failed\n",       \
                                 __FILE__, __LINE__, #expr),                    \
              std::abort()))
+
+#endif
 
 #elif YAO_CONFIG__BUILD_TYPE == YAO_CONFIG__BUILD_TYPE__RELEASE
 
