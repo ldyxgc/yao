c98a548a0af6a07028ddcafcb04d85c4efc8061d
ldyxgc <ldyxgc@163.com>
2024-12-26 02:17:38 +0800

    Add cmd/build.sh

---
+++ cmd/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -x # Print commands and their arguments as they are executed.
+set -e # Exit immediately if a command exits with a non-zero status.
+
+# Config
+cmake -B build
+
+# Build
+cmake --build build
