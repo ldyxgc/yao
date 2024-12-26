4edb766349a78aaa5a69089d460d89da28651d70
ldyxgc <ldyxgc@163.com>
2024-12-27 01:04:44 +0800

    Add time measurement in cmd/build.sh

--- cmd/build.sh
+++ cmd/build.sh
@@ -4,10 +4,13 @@ set -x # Print commands and their arguments as they are executed.
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # Config
+time \
 cmake -B build
 
 # Build
+time \
 cmake --build build
 
 # Test
+time \
 ctest --test-dir build
