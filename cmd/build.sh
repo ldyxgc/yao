eb56837ef27d50e7c96607bc14624738b8725de9
ldyxgc <ldyxgc@163.com>
2024-12-27 04:21:35 +0800

    Add config options for multi-config generators when build and test

--- cmd/build.sh
+++ cmd/build.sh
@@ -5,12 +5,15 @@ set -e # Exit immediately if a command exits with a non-zero status.
 
 # Config
 time \
-cmake -B build
+cmake -B build $@
+
+# Option
+CMAKE_BUILD_TYPE=$(cmake -L -N build | grep CMAKE_BUILD_TYPE | cut -d '=' -f 2)
 
 # Build
 time \
-cmake --build build
+cmake --build build --config ${CMAKE_BUILD_TYPE}
 
 # Test
 time \
-ctest --test-dir build
+ctest --test-dir build --build-config ${CMAKE_BUILD_TYPE}
