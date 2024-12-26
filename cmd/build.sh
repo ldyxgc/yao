a6203fe3140e6ce3ac9b272d3518d64879192d77
ldyxgc <ldyxgc@163.com>
2024-12-27 00:56:19 +0800

    Add config options for multi-config tools in cmd/build.sh

--- cmd/build.sh
+++ cmd/build.sh
@@ -4,10 +4,13 @@ set -x # Print commands and their arguments as they are executed.
 set -e # Exit immediately if a command exits with a non-zero status.
 
 # Config
-cmake -B build
+cmake -B build $@
+
+# Option
+CMAKE_BUILD_TYPE=$(cmake -L -N build | grep CMAKE_BUILD_TYPE | cut -d '=' -f 2)
 
 # Build
-cmake --build build
+cmake --build build --config ${CMAKE_BUILD_TYPE}
 
 # Test
-ctest --test-dir build
+ctest --test-dir build --build-config ${CMAKE_BUILD_TYPE}
