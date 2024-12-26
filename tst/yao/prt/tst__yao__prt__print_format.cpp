533da11f3e496616f2f89c1c1abcdea47b36d7d0
ldyxgc <ldyxgc@163.com>
2024-12-27 00:26:31 +0800

    Add test to yao::prt::print_format()

---
+++ tst/yao/prt/tst__yao__prt__print_format.cpp
@@ -0,0 +1,24 @@
+#include <sstream>
+
+#include "yao/def/check.hpp"
+#include "yao/prt/print_format.hpp"
+
+int main() {
+
+  auto test = [](auto out, auto fmt, auto... args) {
+    std::ostringstream oss;
+    yao::prt::print_format(oss, fmt, args...);
+    return oss.str() == out;
+  };
+
+  YAO_CHECK(test("0x0f", "0x%02x", 0xF));
+  YAO_CHECK(test("0x0F", "0x%02X", 0xf));
+
+  YAO_CHECK(test("0", "0"));
+  YAO_CHECK(test("0 1", "0 %c", '1'));
+  YAO_CHECK(test("0 1 2", "0 %c %s", '1', "2"));
+  YAO_CHECK(test("0 1 2 3", "0 %c %s %i", '1', "2", 3));
+  YAO_CHECK(test("0 1 2 3 4", "0 %c %s %i %.0f", '1', "2", 3, 4.0));
+
+  return 0;
+}
