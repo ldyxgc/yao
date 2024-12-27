6726ca766753e7db91d4b889c6fb757a0d1dfba9
ldyxgc <ldyxgc@163.com>
2024-12-28 01:15:01 +0800

    Replace assert() with YAO_CLAIM() in print_format()

--- src/yao/prt/print_format.cpp
+++ src/yao/prt/print_format.cpp
@@ -1,27 +1,28 @@
 #include "yao/prt/print_format.hpp"
 
-#include <cassert>
 #include <cstdarg>
 #include <cstdio>
 #include <vector>
 
+#include "yao/def/claim.hpp"
+
 namespace yao::prt {
 
 void print_format(std::ostream &os, const char *fmt, ...) {
-  assert(fmt != nullptr);
+  YAO_CLAIM(fmt != nullptr);
   std::va_list args;
 
   va_start(args, fmt);
   int res_num = std::vsnprintf(nullptr, 0, fmt, args);
   va_end(args);
-  assert(res_num >= 0);
+  YAO_CLAIM(res_num >= 0);
 
   std::vector<char> buf(1 + res_num);
 
   va_start(args, fmt);
   int res = std::vsnprintf(buf.data(), buf.size(), fmt, args);
   va_end(args);
-  assert(res >= 0);
+  YAO_CLAIM(res >= 0);
 
   os << buf.data();
 }
