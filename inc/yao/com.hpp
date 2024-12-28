48cf434b1028fb24eefad92bda1697f0057a1bb6
ldyxgc <ldyxgc@163.com>
2024-12-28 15:47:13 +0800

    Export namespace yao::com{} into yao{} in inc/yao/com.hpp

---
+++ inc/yao/com.hpp
@@ -0,0 +1,14 @@
+#ifndef __YAO__COM__HPP__
+#define __YAO__COM__HPP__
+
+#include "yao/com/not_null.hpp"
+#include "yao/com/owner.hpp"
+#include "yao/com/zstring.hpp"
+
+namespace yao {
+
+using namespace yao::com;
+
+} // namespace yao
+
+#endif
