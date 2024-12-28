fb96b4228a80917bb16e8bdbce4e702e6141324c
ldyxgc <ldyxgc@163.com>
2024-12-28 20:23:16 +0800

    Add yao{re{stt{SymbolState{}}}}

---
+++ inc/yao/re/stt/SymbolState.hpp
@@ -0,0 +1,29 @@
+#ifndef __YAO__RE__STT__SYMBOL_STATE__HPP__
+#define __YAO__RE__STT__SYMBOL_STATE__HPP__
+
+namespace yao::re::stt {
+
+template <typename Symbol> class SymbolState {
+public:
+  SymbolState(const Symbol &symbol);
+
+  void match(const Symbol &symbol);
+  bool is_final() const;
+  bool is_dead() const;
+
+private:
+  enum class Label { START, FINAL, DEAD };
+
+private:
+  Symbol _symbol; // const
+  Label _label;
+};
+
+template <typename Symbol>
+SymbolState(const Symbol &symbol) -> SymbolState<Symbol>;
+
+} // namespace yao::re::stt
+
+#include "yao/re/stt/SymbolState.ipp"
+
+#endif
