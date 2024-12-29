996b3400c39a46a24d478f8859b5a26413a011f4
ldyxgc <ldyxgc@163.com>
2024-12-29 19:51:46 +0800

    Add YAO_WARN_{PUSH,POP,OFF__FORMAT_NON_LITERAL} in inc/yao/def/warn.hpp

---
+++ inc/yao/def/warn.hpp
@@ -0,0 +1,30 @@
+#ifndef __YAO__DEF__WARN__HPP__
+#define __YAO__DEF__WARN__HPP__
+
+#include "yao/config.hpp"
+
+#if YAO_CONFIG__COMPILER == YAO_CONFIG__COMPILER__GNU
+
+#define YAO_WARN_PUSH _Pragma("GCC diagnostic push")
+#define YAO_WARN_POP _Pragma("GCC diagnostic pop")
+
+#define YAO_WARN_OFF__FORMAT_NON_LITERAL
+
+#elif YAO_CONFIG__COMPILER == YAO_CONFIG__COMPILER__CLANG
+
+#define YAO_WARN_PUSH _Pragma("clang diagnostic push")
+#define YAO_WARN_POP _Pragma("clang diagnostic pop")
+
+#define YAO_WARN_OFF__FORMAT_NON_LITERAL                                       \
+  _Pragma("clang diagnostic ignored \"-Wformat-nonliteral\"")
+
+#elif YAO_CONFIG__COMPILER == YAO_CONFIG__COMPILER__MSVC
+
+#define YAO_WARN_PUSH _Pragma("warning(push)")
+#define YAO_WARN_POP _Pragma("warning(pop)")
+
+#define YAO_WARN_OFF__FORMAT_NON_LITERAL
+
+#endif
+
+#endif
