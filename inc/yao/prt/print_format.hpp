cd8d977553ef71af06ea3ae7d8b4c308c8558abb
ldyxgc <ldyxgc@163.com>
2024-12-30 14:35:29 +0800

    Add YAO_CONFIG__PRINT_FORMAT in config.hpp, and update print_format()

--- inc/yao/prt/print_format.hpp
+++ inc/yao/prt/print_format.hpp
@@ -1,6 +1,10 @@
 #ifndef __YAO__PRT__PRINT_FORMAT__HPP__
 #define __YAO__PRT__PRINT_FORMAT__HPP__
 
+#include "yao/config.hpp"
+
+#if YAO_CONFIG__PRINT_FORMAT == YAO_CONFIG__PRINT_FORMAT__FUNCTION
+
 #include "yao/prt/impl/func/print_format.hpp"
 
 namespace yao::prt {
@@ -8,5 +12,22 @@ namespace yao::prt {
 constexpr auto print_format = impl::func::print_format;
 
 } // namespace yao::prt
+
+#elif YAO_CONFIG__PRINT_FORMAT == YAO_CONFIG__PRINT_FORMAT__TEMPLATE
+
+#include <ostream>
+
+#include "yao/com.hpp"
+#include "yao/prt/impl/tmpl/print_format.hpp"
+
+namespace yao::prt {
+
+void print_format(std::ostream &os, not_null<czstring> fmt, auto... args) {
+  impl::tmpl::print_format(os, fmt, args...);
+}
+
+} // namespace yao::prt
+
+#endif
 
 #endif
