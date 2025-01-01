8f2cb64461a640f018f1eca49ee6f1bd3b367b89
ldyxgc <ldyxgc@163.com>
2025-01-02 01:30:17 +0800

    Add yao::prt::print_type() for integral types

---
+++ inc/yao/prt/print_type.hpp
@@ -0,0 +1,27 @@
+#ifndef __YAO__PRT__PRINT_TYPE__HPP__
+#define __YAO__PRT__PRINT_TYPE__HPP__
+
+#include <concepts>
+#include <cstdint>
+#include <ostream>
+
+namespace yao::prt {
+
+// ns: namespace, tp: template
+template <typename T, bool ns = false, bool tp = false>
+  requires std::same_as<T, bool> || std::same_as<T, char>
+void print_type(std::ostream &os);
+
+// ns: namespace, tp: template
+template <typename T, bool ns = false, bool tp = false>
+  requires std::same_as<T, std::int8_t> || std::same_as<T, std::uint8_t> ||
+           std::same_as<T, std::int16_t> || std::same_as<T, std::uint16_t> ||
+           std::same_as<T, std::int32_t> || std::same_as<T, std::uint32_t> ||
+           std::same_as<T, std::int64_t> || std::same_as<T, std::uint64_t>
+void print_type(std::ostream &os);
+
+} // namespace yao::prt
+
+#include "yao/prt/print_type.ipp"
+
+#endif
