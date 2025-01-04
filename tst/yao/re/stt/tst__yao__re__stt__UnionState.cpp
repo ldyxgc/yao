0eba040b11f9bbc14d29853f26b812cc1d79d2c6
ldyxgc <ldyxgc@163.com>
2025-01-05 03:54:00 +0800

    Add tests to UnionState{} constructor

--- tst/yao/re/stt/tst__yao__re__stt__UnionState.cpp
+++ tst/yao/re/stt/tst__yao__re__stt__UnionState.cpp
@@ -1,3 +1,6 @@
+#include <concepts>
+#include <utility>
+
 #include "yao/def/check.hpp"
 #include "yao/re/stt/SymbolState.hpp"
 #include "yao/re/stt/UnionState.hpp"
@@ -6,6 +9,33 @@ int main() {
 
   using yao::re::stt::SymbolState;
   using yao::re::stt::UnionState;
+
+  {
+    using TargetState = UnionState<SymbolState<int>, SymbolState<int>>;
+
+    UnionState union_state_a{SymbolState{0}, SymbolState{1}};
+    static_assert(std::same_as<decltype(union_state_a), TargetState>);
+
+    SymbolState<int> lhs_0{0}, lhs_1{1};
+    UnionState union_state_b{lhs_0, lhs_1};
+    static_assert(std::same_as<decltype(union_state_b), TargetState>);
+    UnionState union_state_c{std::move(lhs_0), lhs_1};
+    static_assert(std::same_as<decltype(union_state_c), TargetState>);
+    UnionState union_state_d{lhs_0, std::move(lhs_1)};
+    static_assert(std::same_as<decltype(union_state_d), TargetState>);
+    UnionState union_state_e{std::move(lhs_0), std::move(lhs_1)};
+    static_assert(std::same_as<decltype(union_state_e), TargetState>);
+
+    const SymbolState<int> c_lhs_0{0}, c_lhs_1{1};
+    UnionState union_state_f{c_lhs_0, c_lhs_1};
+    static_assert(std::same_as<decltype(union_state_f), TargetState>);
+    UnionState union_state_g{std::move(c_lhs_0), c_lhs_1};
+    static_assert(std::same_as<decltype(union_state_g), TargetState>);
+    UnionState union_state_h{c_lhs_0, std::move(c_lhs_1)};
+    static_assert(std::same_as<decltype(union_state_h), TargetState>);
+    UnionState union_state_i{std::move(c_lhs_0), std::move(c_lhs_1)};
+    static_assert(std::same_as<decltype(union_state_i), TargetState>);
+  }
 
   { // 0|1
     UnionState union_state{SymbolState{0}, SymbolState{1}};
