a66e81358f3047572d39e2278a072cdde28a9f96
ldyxgc <ldyxgc@163.com>
2025-01-06 20:43:32 +0800

    Add yao::com{not_null<>,owner<>,zstring,czstring}, and export them to yao{}

---
+++ inc/yao/com/zstring.hpp
@@ -0,0 +1,12 @@
+#ifndef __YAO__COM__ZSTRING__HPP__
+#define __YAO__COM__ZSTRING__HPP__
+
+namespace yao::com {
+
+// Zero-terminated string (C-style string)
+using zstring = char *;
+using czstring = const char *;
+
+} // namespace yao::com
+
+#endif
