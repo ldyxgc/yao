f3804c059c455947b70f2a1a0bf0e09837dc727b
ldyxgc <ldyxgc@163.com>
2025-01-09 00:57:40 +0800

    Select stt::ErasedState according to YAO_CONFIG__RE_ERASED_STATE

--- inc/yao/re/stt/ErasedState.hpp
+++ inc/yao/re/stt/ErasedState.hpp
@@ -1,6 +1,10 @@
 #ifndef __YAO__RE__STT__ERASED_STATE__HPP__
 #define __YAO__RE__STT__ERASED_STATE__HPP__
 
+#include "yao/config.hpp"
+
+#if YAO_CONFIG__RE_ERASED_STATE == YAO_CONFIG__RE_ERASED_STATE__VIRTUAL_FUNCTION
+
 #include "yao/re/stt/impl/vfun/ErasedState.hpp"
 
 namespace yao::re::stt {
@@ -8,5 +12,18 @@ namespace yao::re::stt {
 using impl::vfun::ErasedState;
 
 } // namespace yao::re::stt
+
+#elif YAO_CONFIG__RE_ERASED_STATE ==                                           \
+    YAO_CONFIG__RE_ERASED_STATE__FUNCTION_OBJECT
+
+#include "yao/re/stt/impl/fobj/ErasedState.hpp"
+
+namespace yao::re::stt {
+
+using impl::fobj::ErasedState;
+
+} // namespace yao::re::stt
+
+#endif
 
 #endif
