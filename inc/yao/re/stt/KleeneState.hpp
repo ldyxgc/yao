abfedec0f7ade253be9d12481f1380333e422709
ldyxgc <ldyxgc@163.com>
2025-01-11 01:51:08 +0800

    Suppress padding warnings in KleeneState{}

--- inc/yao/re/stt/KleeneState.hpp
+++ inc/yao/re/stt/KleeneState.hpp
@@ -3,11 +3,15 @@
 
 #include <set>
 
+#include "yao/def/warn.hpp"
 #include "yao/re/stt/StateBase.hpp"
 #include "yao/re/stt/c_ct_State.hpp"
 #include "yao/req/c_r_no_cvref.hpp"
 
 namespace yao::re::stt {
+
+YAO_WARN_PUSH
+YAO_WARN_OFF__PADDING
 
 template <typename SubState>
   requires req::c_r_no_cvref<SubState> && c_ct_State<SubState>
@@ -30,6 +34,8 @@ private:
   std::set<SubState> _sub_state_set;
   bool _is_final;
 };
+
+YAO_WARN_POP
 
 } // namespace yao::re::stt
 
