8a476ca489172fcd70133e23737a92fae53a750e
ldyxgc <ldyxgc@163.com>
2025-01-15 00:28:00 +0800

    Add cmd/{build,debug,release}.sh and .github/workflows/build.yml

---
+++ cmd/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x # Print commands and their arguments as they are executed.
+set -e # Exit immediately if a command exits with a non-zero status.
+
+# Config
+time \
+cmake -B build $@
+
+# Option
+CMAKE_BUILD_TYPE=$(cmake -L -N build | grep CMAKE_BUILD_TYPE | cut -d '=' -f 2)
+
+# Build
+time \
+cmake --build build --config ${CMAKE_BUILD_TYPE} --parallel
+
+# Test
+time \
+ctest --test-dir build --build-config ${CMAKE_BUILD_TYPE} --timeout 5 --parallel
