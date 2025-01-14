580a50282c75436c83176c1aaa3193a03b884f08
ldyxgc <ldyxgc@163.com>
2025-01-15 02:35:37 +0800

    Add integral aliases in inc/yao/com/integral.hpp

---
+++ inc/yao/com/integral.hpp
@@ -0,0 +1,23 @@
+#ifndef __YAO__COM__INTEGRAL__HPP__
+#define __YAO__COM__INTEGRAL__HPP__
+
+namespace yao::com {
+
+using schar = signed char;
+using uchar = unsigned char;
+
+// short
+using ushort = unsigned short;
+
+// int
+using uint = unsigned;
+
+// long
+using ulong = unsigned long;
+
+using llong = long long;
+using ullong = unsigned long long;
+
+} // namespace yao::com
+
+#endif
