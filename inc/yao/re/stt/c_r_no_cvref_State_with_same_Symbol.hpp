43a4d0068ae026466d776c20d7cfe66f0226077d
ldyxgc <ldyxgc@163.com>
2025-01-18 02:36:25 +0800

    Add yao::re::stt{c_r_{,{no,rm}_}cvref_State_with_same_Symbol<>}

---
+++ inc/yao/re/stt/c_r_no_cvref_State_with_same_Symbol.hpp
@@ -0,0 +1,17 @@
+#ifndef __YAO__RE__STT__C_R_NO_CVREF_STATE_WITH_SAME_SYMBOL__HPP__
+#define __YAO__RE__STT__C_R_NO_CVREF_STATE_WITH_SAME_SYMBOL__HPP__
+
+#include "yao/re/stt/c_r_State_with_same_Symbol.hpp"
+#include "yao/req/c_r_no_cvref.hpp"
+
+namespace yao::re::stt {
+
+// c: concept, r: require
+template <typename LhsState, typename RhsState>
+concept c_r_no_cvref_State_with_same_Symbol =
+    req::c_r_no_cvref<LhsState> && req::c_r_no_cvref<RhsState> &&
+    c_r_State_with_same_Symbol<LhsState, RhsState>;
+
+} // namespace yao::re::stt
+
+#endif
