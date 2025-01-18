e4a31272288cf08dbe8dc20c8752831bc5a658ae
ldyxgc <ldyxgc@163.com>
2025-01-19 00:07:13 +0800

    Add yao::com{not_null<>,owner<>}

--- inc/yao/com.hpp
+++ inc/yao/com.hpp
@@ -2,6 +2,8 @@
 #define __YAO__COM__HPP__
 
 #include "yao/com/integral.hpp"
+#include "yao/com/not_null.hpp"
+#include "yao/com/owner.hpp"
 
 namespace yao {
 
--- tst/yao/tst__yao__com.cpp
+++ tst/yao/tst__yao__com.cpp
@@ -22,6 +22,26 @@ static_assert(std::is_integral_v<ulong>);
 static_assert(std::is_integral_v<llong>);
 static_assert(std::is_integral_v<ullong>);
 
+// -----------------------------------------------------------------------------
+
+template <typename T>
+concept test__not_null = requires { not_null<T>{}; };
+
+static_assert(test__not_null<int *>);
+static_assert(test__not_null<const int *>);
+static_assert(test__not_null<std::unique_ptr<int>>);
+static_assert(test__not_null<std::unique_ptr<const int>>);
+static_assert(test__not_null<std::shared_ptr<int>>);
+static_assert(test__not_null<std::shared_ptr<const int>>);
+
+// -----------------------------------------------------------------------------
+
+template <typename T>
+concept test__owner = requires { owner<T>{}; };
+
+static_assert(test__owner<int *>);
+static_assert(test__owner<const int *>);
+
 } // namespace yao
 
 int main() { return 0; }
